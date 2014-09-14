950e574534fb96a57a4fe58ecba51950888dec05
acosinwork <acosinwork@yandex.ru>
2014-09-14 11:57:05 +0400

    add some architecture

---
+++ tetra_hardware/tetra_hardware.h
@@ -0,0 +1,5 @@
+#include <inttypes.h>
+#include <arduino.h>
+
+
+
---
+++ tetra_hardware/troyka/troyka.h
@@ -0,0 +1,18 @@
+#include <arduino.h>
+#include <inttypes.h>
+
+class troyka()
+{
+
+public:
+    
+    init(uint8_t pin); 
+    
+    
+private:
+
+    uint8_t _pin;
+
+    
+    
+}
